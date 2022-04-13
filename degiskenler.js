bbb2071564d2a382893b6074df7d311135cf0dae
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-04-14 01:43:36 +0300

    Değişken Tanımlama

---
+++ degiskenler.js
@@ -0,0 +1,25 @@
+var metin =  "Java Script Öğreniyorum";
+var sayi = 2435345;
+//Değişken tanımlarken bütün veri tipleri için "var"ı kullanıyoruz.
+
+console.log(metin);
+console.log(sayi);
+
+
+// Değişken isimleri sayı ile başlamaz. String bir ifade olmak zorunda.
+//var 5sayi = 5;
+
+//Özel tanımlı keyword'ler de değişken ismi olarak kullanılamaz.
+//var new = "yeni isim";
+
+//Değişken tanımlarken ilk kelimenin baş harfi küçük diğer kelimelerin baş harfleri büyük olarak yazılır. Bu bir camel case'dir.
+ var yeniBirDegisken = "Değişken tanımlarken bu kurallara dikkat etmek okunabilirliği arttırır.";
+
+ console.log(yeniBirDegisken);
+
+ //Çoklu değişken tanımlayabiliriz.
+ var a = 3,
+    b = 33,
+    c = 1;
+
+  console.log(a + b + c);
