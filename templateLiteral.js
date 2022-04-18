d4f1d36998db16d69fe91749c29bd78cf76d11d6
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-04-19 01:50:30 +0300

    Temlate Literal

---
+++ templateLiteral.js
@@ -0,0 +1,6 @@
+const ad = "Ayşe";
+const surname = "Yıldız";
+
+console.log (ad + " " + surname) //es6'dan önce
+
+console.log(`Adım : ${ad}, Soyadım : ${surname}`); //Bu şekide daha kolay bir kullanım sağlıyor.
