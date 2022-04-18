097b067cd5ed9674c475dd1590291f81359d7135
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-04-19 02:31:21 +0300

    Use Strict

--- useStrict.js
+++ useStrict.js
@@ -1,4 +1,4 @@
-use strict; //Doğru yapmaya zorla anlamına geliyor. Hata yaptığında uyarır.
+"use strict"; //Doğru yapmaya zorla anlamına geliyor. Hata yaptığında uyarır.
 
 data = 11; //Yeni sürümde bu hataya sebep olur.
 console.log(data);
