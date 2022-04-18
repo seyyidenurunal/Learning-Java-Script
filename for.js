dde50ef05edaee328410fdb763a57e73e3b41c2a
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-04-18 14:27:15 +0300

    For Döngüsü

---
+++ for.js
@@ -0,0 +1,6 @@
+//Döngüler, bir işlemi aynı kodu tekrarlamadan tekrar etmeyi sağlar.
+
+for (var i = 0; i < 10; i++) { //Sırayla; değişken, koşul, artış miktarı.
+    console.log(i);
+}
+//Bunu yapmamış olsak, 0'dan 9'a kadar rakamları tek tek console log ile yazacaktık.
